72f02a95037a688d3fbb637ec12f8971fd0989b8
shaggyshelar <shaggy.shelar@gmail.com>
2016-11-16 14:47:36 +0530

    Changed URL of some png files and added userData server file

--- src/server/apis.js
+++ src/server/apis.js
@@ -1,4 +1,25 @@
+var utils = require('./utils');
+var users = require('./userData');
+var _ = require('lodash');
+
 module.exports = function (app) {
+    app.post('/api/Authentication/GetToken', function (req, res) {
+        var userName = req.body.UserName;
+        var password = req.body.Password;
+        var userIndex = _.findIndex(users, { UserName: userName, Password: password });
+        if (userIndex != '-1') {
+            var token = utils.CreateJWT(users[userIndex]);
+            res.send({ token: token });
+        } else {
+            res.status(500).end('Invalid credentials.');
+        }
+    });
+    
+    app.get('/api/GetLoggedInUserPermission', utils.EnsureAuthenticated, function (req, res) {
+        var userIndex = _.findIndex(users, { Id: req.userID });
+        res.json(users[userIndex].Permissions);
+    });
+
     app.get('/api/getHolidays', function (req, res) {
         res.json([
             { id: '1111', date: '11/11/1111', title: 'Laxmi Puja', type: 'Fixed' },
@@ -7,4 +28,13 @@ module.exports = function (app) {
             { id: '4444', date: '44/44/1111', title: 'New Year', type: 'Floating' },
         ]);
     });
-}
+
+    app.get('/api/getTimesheets', function (req, res) {
+        res.json([
+            { id: '11111', date: '11/11/1111', billableHours: '11:11', nonBillableHours: '12:12', name: 'Records 1' },
+            { id: '22222', date: '22/11/1111', billableHours: '22:11', nonBillableHours: '22:12', name: 'Records 1' },
+            { id: '3333', date: '33/11/1111', billableHours: '33:11', nonBillableHours: '33:12', name: 'Records 1' },
+            { id: '4444', date: '44/11/1111', billableHours: '44:11', nonBillableHours: '44:12', name: 'Records 1' },
+        ]);
+    });
+};
---
+++ src/server/userData.js
@@ -0,0 +1,71 @@
+var users = [
+    {
+        Id: 10,
+        UserName: 'admin',
+        Password: 'password',
+        Permissions: [
+            'Timesheet.READ', 'Timesheet.CREATE', 'Timesheet.UPDATE', 'Timesheet.DELETE',
+            'ConferenceBook.READ', 'ConferenceBook.CREATE', 'ConferenceBook.UPDATE', 'ConferenceBook.DELETE',
+            'Leave.READ', 'Leave.CREATE', 'Leave.UPDATE', 'Leave.DELETE',
+            'Holiday.READ', 'Holiday.CREATE', 'Holiday.UPDATE', 'Holiday.DELETE',
+            'LeaveApproval.READ', 'LeaveApproval.CREATE', 'LeaveApproval.UPDATE', 'LeaveApproval.DELETE',
+            'BulkLeaveApproval.READ', 'BulkLeaveApproval.CREATE', 'BulkLeaveApproval.UPDATE', 'BulkLeaveApproval.DELETE', 'Ticket.READ'
+        ]
+    },
+    {
+        Id: 11,
+        UserName: 'user1',
+        Password: 'password',
+        Permissions: ['Leave.READ', 'Leave.CREATE', 'Leave.UPDATE', 'Leave.DELETE']
+    },
+    {
+        Id: 12,
+        UserName: 'user2',
+        Password: 'password',
+        Permissions: ['Holiday.READ', 'Holiday.CREATE', 'Holiday.UPDATE', 'Holiday.DELETE',]
+    },
+    {
+        Id: 13,
+        UserName: 'user3',
+        Password: 'password',
+        Permissions: ['ConferenceBook.READ', 'ConferenceBook.CREATE', 'ConferenceBook.UPDATE', 'ConferenceBook.DELETE']
+    },
+
+    {
+        Id: 14,
+        UserName: 'user4',
+        Password: 'password',
+        Permissions: ['Leave.READ', 'Holiday.READ', 'Leave.CREATE', 'LeaveApproval.READ', 'BulkLeaveApproval.READ']
+    },
+    {
+        Id: 15,
+        UserName: 'user5',
+        Password: 'password',
+        Permissions: ['Leave.READ', 'Leave.CREATE']
+    },
+    {
+        Id: 16,
+        UserName: 'user6',
+        Password: 'password',
+        Permissions: ['Leave.READ', 'LeaveApproval.READ', 'BulkLeaveApproval.READ', 'BulkLeaveApproval.UPDATE']
+    },
+    {
+        Id: 17,
+        UserName: 'user7',
+        Password: 'password',
+        Permissions: ['Leave.READ', 'Holiday.READ', 'LeaveApproval.READ']
+    },
+    {
+        Id: 18,
+        UserName: 'user8',
+        Password: 'password',
+        Permissions: ['Timesheet.READ', 'Timesheet.CREATE', 'Timesheet.UPDATE', 'Timesheet.DELETE']
+    },
+    {
+        Id: 19,
+        UserName: 'user9',
+        Password: 'password',
+        Permissions: ['Leave.READ', 'Leave.CREATE', 'Timesheet.READ', 'Timesheet.CREATE']
+    }
+];
+module.exports = users;
