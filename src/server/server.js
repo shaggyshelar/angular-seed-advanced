6fe54936c9b03e5f4b6bd1417c78a30f65806a7c
shaggyshelar <shaggy.shelar@gmail.com>
2016-11-17 11:16:34 +0530

    Added custom template related js files- app, demo, layout, quick-sidebar

--- src/server/server.js
+++ src/server/server.js
@@ -10,24 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./apis')(app);
 
-// Remove below code on production
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5555');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.sendStatus(200);
-    }
-    else {
-        next();
-    }
-};
-app.use(allowCrossDomain);
-// Remove above code on production
-
 app.get('/', function (req, res) {
     res.render('index.html');
 });
