e2ac2125262c04d93bee0bff91b7d394b6ebeffb
Salauddin <sagar.shelar@eternussolutions.com>
2016-11-22 20:44:39 +0530

    Implemented CRUD operation for Role in admin module

--- src/client/app/components/admin/admin.module.ts
+++ src/client/app/components/admin/admin.module.ts
@@ -9,17 +9,20 @@ import {CommonModule, TranslateModule} from '../../shared/index';
 
 /** Module level Dependencies */
 import { FeatureComponent } from './components/feature/feature.component';
+import { RoleListComponent } from './components/roles/role-list/role-list.component';
+import { RoleAddEditComponent } from './components/roles/role-add-edit/role-add-edit.component';
 import { AdminRoutingModule } from './admin.routes';
 import { FeatureService } from './services/feature.service';
+import { RoleService } from './services/role.service';
 
 /** Module Import Declarations */
 let imports = [AdminRoutingModule,RouterModule,FormsModule,BrowserModule,CommonModule, TranslateModule];
 
 /** Component/Directive Declarations */
-let declarations = [FeatureComponent];
+let declarations = [FeatureComponent,RoleListComponent,RoleAddEditComponent];
 
 /** Providers Declarations*/
-let providers = [FeatureService];
+let providers = [FeatureService,RoleService];
 
 /** Module Definition */
 @NgModule({
