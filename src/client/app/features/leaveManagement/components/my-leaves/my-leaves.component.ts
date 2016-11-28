ebda2aa6562e011941603e03e786810806f534f7
MT <tilak.mukul@hotmail.com>
2016-11-28 20:29:12 +0530

    reducer implementation my-leaves

--- src/client/app/features/leaveManagement/components/my-leaves/my-leaves.component.ts
+++ src/client/app/features/leaveManagement/components/my-leaves/my-leaves.component.ts
@@ -22,21 +22,23 @@ import { LEAVE_ACTIONS } from '../../services/leave.actions';
 export class MyLeavesComponent {
   public leaveObs: Observable<any>;
   servRows = 5;
-  leaves: any[];
-  leave :any;
+  leaves: {};
+  leave: any;
 
   constructor(
-    private router: Router,private store: Store<any>, private logService: LogService
-  ) { 
-     this.leaves = [];
+    private router: Router, private store: Store<any>, private logService: LogService
+  ) {
+    this.leaves = [];
   }
 
   ngOnInit() {
     this.store.dispatch({ type: LEAVE_ACTIONS.DETAILS, payload: 1 });
     this.leaveObs = this.store.select('leave');
-    this.leaveObs.subscribe(res =>
-      this.leaves = res ? res.leaves : []
+    this.leaveObs.subscribe(res => {
+      this.leave = res ? this.arrangeData(res.leaves) : []
+    }
     );
+
     // this.leaves = [
     //   { empName: 'Person1 LName', start: '22-09-2016', end: '23-09-2016', numDays: 2, status: 'Approved' },
     //   { empName: 'Person1 LName', start: '22-08-2016', end: '22-08-2016', numDays: 1, status: 'Approved' },
@@ -50,5 +52,11 @@ export class MyLeavesComponent {
 
   updateBtnClicked(id) {
     this.router.navigate(['/leave/update-leave', id]);
+  }
+
+  arrangeData(leaveParam) {
+    // TODO : Convert response into flat object
+    if (leaveParam)
+      console.log('from my-leaves data : ' + JSON.stringify(leaveParam));
   }
 }
