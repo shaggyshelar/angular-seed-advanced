ad56ea15c9f3faef2fcc86b54e997e866ad67009
MT <tilak.mukul@hotmail.com>
2016-11-28 15:18:38 +0530

    leave api added

---
+++ src/server/leave/leave.js
@@ -0,0 +1,44 @@
+var utils = require('../utils');
+var leaves = require('./leaveData');
+var _ = require('lodash');
+
+module.exports = function (app) {
+    app.get('/api/leave/:id', function (req, res) {
+        var id = req.params.id;
+        for (var index in leaves) {
+            if (leaves[index].id == id) {
+                res.json(leaves[index]);
+                return;
+            }
+        }
+    });
+
+    app.get('/api/leave', function (req, res) {
+        res.json(leaves);
+    });
+
+    app.post('/api/leave', function (req, res) {
+        if (req.body != null) {
+            leaves.push(req.body);
+            res.sendStatus(201);
+        } else {
+            res.status(500).end('Bad Request.');
+        }
+    });
+
+    app.put('/api/leave', function (req, res) {
+        if (req.body != null && req.params.id) {
+            var id = req.params.id;
+            for (var index in leaves) {
+                if (leaves[index].id == id) {
+                    leaves[index] = req.body;
+                    break;
+                }
+            }
+            res.sendStatus(201);
+        }
+        else {
+            res.status(500).end('Bad Request.');
+        }
+    });
+}
--- src/server/leave/leaveData.js
+++ src/server/leave/leaveData.js
@@ -1,55 +1,263 @@
 // List of hardcoded features along with their details.
 var leaves = [
-        {
-            id: 1,
-            empName: 'Mukul',
-            empId: 10431,
-            project: 'Linkup',
-            approvers: 'Sagar Shelar',
-            pending: 'Sagar Shelar',
-            status: 'Pending',
-            leaveType: 'Leave',
-            numDays: 2,
-            start: '24-11-2016',
-            end: '25-11-2016',
-            reason: 'Bad health'
-        }, {
-            id: 2,
-            empName: 'Mukul',
-            empId: 10431,
-            project: 'Linkup',
-            approvers: 'Sagar Shelar',
-            pending: 'Approved',
-            leaveType: 'Leave',
-            numDays: 2,
-            start: '24-11-2015',
-            end: '25-11-2015',
-            reason: 'Bad health'
-        }, {
-            id: 3,
-            empName: 'Mukul',
-            empId: 10431,
-            project: 'Linkup',
-            approvers: 'Sagar Shelar',
-            pending: 'Approved',
-            leaveType: 'Leave',
-            numDays: 2,
-            start: '4-1-2017',
-            end: '5-1-2017',
-            reason: 'Personal'
-        }, {
-            id: 4,
-            empName: 'Mukul',
-            empId: 10431,
-            project: 'Linkup',
-            approvers: 'Sagar Shelar',
-            pending: 'Approved',
-            leaveType: 'Leave',
-            numDays: 1,
-            start: '12-9-2015',
-            end: '12-9-2015',
-            reason: 'Birthday celebration'
-        }
-    ];
+    {
+        leaveLst: [
+            {
+                user: {
+                    id: 10431,
+                    name: 'Mukul Tilak',
+                    project: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar',
+                        },
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    approvedBy: [
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    pendingApprovers: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar'
+                        }
+                    ]
+                },
+                startDate: new Date(2016, 11, 28, 00, 00, 00, 00),
+                endDate: new Date(2016, 11, 28, 00, 00, 00, 00),
+                numberOfLeave: 1,
+                status: 'Approved',
+                reason: 'Personal'
+            },
+            {
+                user: {
+                    id: 10431,
+                    name: 'Mukul Tilak',
+                    project: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar',
+                        },
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    approvedBy: [
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    pendingApprovers: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar'
+                        }
+                    ]
+                },
+                startDate: new Date(2016, 11, 29, 00, 00, 00, 00),
+                endDate: new Date(2016, 11, 29, 00, 00, 00, 00),
+                numberOfLeave: 1,
+                status: 'Approved',
+                reason: 'Personal'
+            }
+        ],
+        numberOfLeave: 2,
+        halfDayLeave: 0,
+        absent: 0,
+        halfDayAbsent: 0,
+        id: 1
+    },
+    {
+        leaveLst: [
+            {
+                user: {
+                    id: 10431,
+                    name: 'Mukul Tilak',
+                    project: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar',
+                        },
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    approvedBy: [
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    pendingApprovers: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar'
+                        }
+                    ]
+                },
+                startDate: new Date(2016, 11, 29, 00, 00, 00, 00),
+                endDate: new Date(2016, 12, 29, 00, 00, 00, 00),
+                numberOfLeave: 2,
+                status: 'Approved',
+                reason: 'Personal'
+            },
+            {
+                user: {
+                    id: 10431,
+                    name: 'Mukul Tilak',
+                    project: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar',
+                        },
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    approvedBy: [
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    pendingApprovers: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar'
+                        }
+                    ]
+                },
+                startDate: new Date(2016, 11, 30, 00, 00, 00, 00),
+                endDate: new Date(2016, 12, 30, 00, 00, 00, 00),
+                numberOfLeave: 2,
+                status: 'Approved',
+                reason: 'Personal'
+            }
+        ],
+        numberOfLeave: 2,
+        halfDayLeave: 0,
+        absent: 0,
+        halfDayAbsent: 0,
+        id: 2
+    },
+    {
+        leaveLst: [
+            {
+                user: {
+                    id: 10431,
+                    name: 'Mukul Tilak',
+                    project: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar',
+                        },
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    approvedBy: [
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    pendingApprovers: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar'
+                        }
+                    ]
+                },
+                startDate: new Date(2017, 1, 04, 00, 00, 00, 00),
+                endDate: new Date(2017, 1, 04, 00, 00, 00, 00),
+                numberOfLeave: 2,
+                status: 'Approved',
+                reason: 'Personal'
+            },
+            {
+                user: {
+                    id: 10431,
+                    name: 'Mukul Tilak',
+                    project: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar',
+                        },
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    approvedBy: [
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    pendingApprovers: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar'
+                        }
+                    ]
+                },
+                startDate: new Date(2017, 01, 05, 00, 00, 00, 00),
+                endDate: new Date(2017, 01, 05, 00, 00, 00, 00),
+                numberOfLeave: 2,
+                status: 'Approved',
+                reason: 'Personal'
+            },
+            {
+                user: {
+                    id: 10431,
+                    name: 'Mukul Tilak',
+                    project: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar',
+                        },
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    approvedBy: [
+                        {
+                            title: 'EBS',
+                            manager: 'Kunal Adhikari'
+                        }
+                    ],
+                    pendingApprovers: [
+                        {
+                            title: 'Angular 2',
+                            manager: 'Sagar Shelar'
+                        }
+                    ]
+                },
+                startDate: new Date(2017, 01, 06, 00, 00, 00, 00),
+                endDate: new Date(2016, 01, 06, 00, 00, 00, 00),
+                numberOfLeave: 2,
+                status: 'Approved',
+                reason: 'Personal'
+            }
+        ],
+        numberOfLeave: 2,
+        halfDayLeave: 0,
+        absent: 0,
+        halfDayAbsent: 0,
+        id: 3
+    }
+];
 
 module.exports = leaves;
