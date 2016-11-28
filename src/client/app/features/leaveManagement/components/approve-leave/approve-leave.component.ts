b025dcb56c712ba11271799b4a4a62cc583f6d6d
MT <tilak.mukul@hotmail.com>
2016-11-28 20:27:48 +0530

    reducer implementation approve-leave

--- src/client/app/features/leaveManagement/components/approve-leave/approve-leave.component.ts
+++ src/client/app/features/leaveManagement/components/approve-leave/approve-leave.component.ts
@@ -1,25 +1,19 @@
 /** Angular Dependencies */
 import { Router } from '@angular/router';
-
 /** Framework Dependencies */
-import { BaseComponent } from '../views/base-component';
+//import { BaseComponent } from '../views/base-component';
+import { BaseComponent, LogService } from '../../../framework.ref';
+
+/** Third Party Dependencies */
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
 
 /** Module Level Dependencies */
+import { LEAVE_ACTIONS } from '../../services/leave.actions';
 
 /** Component Declaration */
 
 import { OnInit } from '@angular/core';
-
-export class ShowLeaveReq {
-  eid: number;
-  employee: string;
-  numberofleaves: number;
-  status: string;
-  start: string;
-  end: string;
-  approvers: string;
-  pending: string;
-}
 
 @BaseComponent({
   moduleId: module.id,
@@ -28,11 +22,14 @@ export class ShowLeaveReq {
 })
 export class ApproveLeaveComponent implements OnInit {
 
-  approvalRecords: ShowLeaveReq[];
+  leaveObs: Observable<any>
+  approvalRecords: any[];
   servRows = 10;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private store: Store<any>,
+    private logService: LogService
   ) { }
 
   ngOnInit() {
@@ -41,6 +38,13 @@ export class ApproveLeaveComponent implements OnInit {
       { eid: 23124, employee: 'Employee', numberofleaves: 4, status: 'Approved', start: '01-10-2016', end: '10-10-2016', approvers: 'Manager, Manager, Manager, Manager', pending: '' }
     ];
 
+    this.store.dispatch({ type: LEAVE_ACTIONS.DETAILS, payload: 1 });
+    this.leaveObs = this.store.select('leave');
+    this.leaveObs.subscribe(res =>{
+      this.approvalRecords = res ? res.leaves : [];
+      if (res)
+        console.log('from approve-leave data: ' + JSON.stringify(res.leaves));
+    });
   }
 
 
