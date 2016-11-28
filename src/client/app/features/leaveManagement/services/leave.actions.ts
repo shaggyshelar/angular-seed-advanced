8d1304ca3ee5c039c541c02d10e441fed11fd222
MT <tilak.mukul@hotmail.com>
2016-11-28 20:30:23 +0530

    new actions added for Holiday

--- src/client/app/features/leaveManagement/services/leave.actions.ts
+++ src/client/app/features/leaveManagement/services/leave.actions.ts
@@ -13,6 +13,8 @@ interface ILeaveActions {
     DETAILS_FETCHED: string;
     DETAIL: string;
     DETAIL_FETCHED: string;
+    HOLIDAYS: string;
+    HOLIDAYS_FETCHED: string;
 }
 
 export const LEAVE_ACTIONS: ILeaveActions = {
@@ -21,4 +23,6 @@ export const LEAVE_ACTIONS: ILeaveActions = {
     DETAILS_FETCHED : `${CONTEXT}_DELAILS_FETCHED`,
     DETAIL: `${CONTEXT}_DELAIL`,
     DETAIL_FETCHED: `${CONTEXT}_DELAIL_FETCHED`,
+    HOLIDAYS: `${CONTEXT}_HOLIDAYS`,
+    HOLIDAYS_FETCHED: `${CONTEXT}_HOLIDAYS_FETCHED`,
 };
