5fa50742c43a3a29757ab9d2f64a370e60ba2b08
Mahesh Kedari <Mahesh Kedari>
2016-11-28 15:35:33 +0530

    Basic Authentication Layer added 1. Routers are protected with guard 2. Simple Authentication form added

--- src/client/app/features/core/auth/auth.component.ts
+++ src/client/app/features/core/auth/auth.component.ts
@@ -4,6 +4,8 @@ import { OnInit } from '@angular/core';
 /** Framework Dependencies */
 import { BaseComponent, LogService } from '../../framework.ref';
 
+import { AuthService } from './auth.service';
+
 /** Component Definition*/
 @BaseComponent({
     moduleId: module.id,
@@ -11,11 +13,19 @@ import { BaseComponent, LogService } from '../../framework.ref';
     templateUrl: 'auth.component.html'
 })
 export class AuthComponent implements OnInit {
-    constructor(private logService: LogService) {
+    constructor(private logService: LogService, private authService: AuthService) {
         logService.debug('AuthComponent : constructor');
     }
 
     ngOnInit() {
         this.logService.debug('AuthComponent : ngOnInit');
     }
+
+    login() {
+        if (this.authService.authenticate('', '')) {
+            console.log('Auth Successful');
+            //Perform Post authentication cleanup tasks
+        };
+    }
+
 }
--- src/client/app/features/features.routes.ts
+++ src/client/app/features/features.routes.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 //Components
 import { DashboardComponent, DashboardContainerComponent } from './core/dashboard/index';
 //Modules
-import { ErrorPagesRoutes } from './core/index';
+import { ErrorPagesRoutes, AuthGuard } from './core/index';
 
 import { UsersRoutes, UserModule } from './users/index';
 import { TimesheetRoutes, TimesheetModule } from './timesheet/index';
@@ -22,12 +22,14 @@ export const featureRoutes: Routes = [
     {
         path: 'user',
         children: [...UsersRoutes],
+        canActivate: [AuthGuard],
         data: {
             reducers: UserModule.reducers()
         }
     }, {
         path: 'timesheet',
         children: [...TimesheetRoutes],
+        canActivate: [AuthGuard],
         data: {
             reducers: TimesheetModule.reducers()
         }
@@ -47,6 +49,7 @@ const routes: Routes = [
     {
         path: 'app',
         component: DashboardContainerComponent,
+        canActivate: [AuthGuard],
         children: [
             ...featureRoutes,
             ...ErrorPagesRoutes,
