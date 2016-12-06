9f7f0c77aedb845770511b330e0dfa633ca27b60
MT <tilak.mukul@hotmail.com>
2016-12-06 17:39:55 +0530

    authGuard, CRUD permissions added

--- src/client/app/features/changePassword/change-password.routes.ts
+++ src/client/app/features/changePassword/change-password.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 
 /** Module Level Depedencies */
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { AuthGuard } from '../core/index';
 
 export const ChangePasswordRoutes: Routes = [
   {
@@ -12,6 +13,10 @@ export const ChangePasswordRoutes: Routes = [
   },
   {
     path: 'change',
-    component: ChangePasswordComponent
+    component: ChangePasswordComponent,
+    canActivate: [AuthGuard],
+    data: {
+      permissions: ['PASSWORD.UPDATE']
+    }
   }
 ];
